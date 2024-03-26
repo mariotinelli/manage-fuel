4f6c0b7dcb676ef65e1acb6505b7a5e4d3d45ad6
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-03-26 20:52:04 -0300

    created layout of the homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,25 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './app/Livewire/**/*.php',
     ],
-
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'primary-green': '#7DB563',
+                'primary-green-light': '#A8CF45CC',
+                'primary-blue': '#02A6A5',
+                'primary-blue-light': '#02A6A5CC',
+            }
         },
     },
-
     plugins: [forms],
 };
