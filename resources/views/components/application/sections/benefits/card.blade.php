e280da56362e76d8e0e368e482f578491429f3a4
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-03-27 15:42:03 -0300

    created resources section on the homepage

--- resources/views/components/application/sections/benefits/card.blade.php
+++ resources/views/components/application/sections/benefits/card.blade.php
@@ -7,9 +7,12 @@
     class="flex flex-col gap-5 text-gray-900 dark:text-zinc-200 rounded-3xl shadow-sm p-8 bg-white dark:bg-gray-800 dark:border max-w-[28rem]"
 >
 
-    <span class="text-2xl font-bold" >
-        {{ $title }}
-    </span >
+    <div class="flex items-center gap-3" >
+        <x-icons.arrow-trending-up class="text-green-600" />
+        <span class="text-2xl font-bold" >
+            {{ $title }}
+        </span >
+    </div >
 
     <p class="text-gray-500 dark:text-zinc-200" >
         {{ $description }}
